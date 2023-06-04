d59fd20df8cd4d2a4f17218435605b655c85178f
me08dushyant <me08dushyant@gmail.com>
2023-06-04 14:15:35 +0530

    update

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      white: "#ffffff",
+      purple: "#551A79",
+      orange: "#F16923",
+      gray: colors.gray,
+      violet: colors.violet,
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      inter: ["Inter"],
+    },
+
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
