27fc16d95c7d1afc7515103db0ebe6f445cfe7d5
me08dushyant <me08dushyant@gmail.com>
2024-03-18 00:59:51 +0530

    update

--- src/components/VRHeader.jsx
+++ src/components/VRHeader.jsx
@@ -1,12 +1,11 @@
-
-const VRHeader = () => {
-
-    return (<>
-        <iframe class="h-full w-full bg-orange " src="/header/vtour/index.html"> 
-            VR HEader
-        </iframe>
+const VRHeader = ({ tourLink }) => {
+  return (
+    <>
+      <iframe class="h-full w-full min-h-[400px]  " src={tourLink}>
+        VR HEader
+      </iframe>
     </>
-    )
-}
+  );
+};
 
 export default VRHeader;
